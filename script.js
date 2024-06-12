882617efc607b960c3ba4ec04ce0530937de08dd
Joe Chek <jmchek@gmail.com>
2024-06-12 15:35:58 -0700

    add the getTemperature() and createWeatherObj() functions to script.js

--- script.js
+++ script.js
@@ -8,4 +8,32 @@ async function getWeather(location) {
     console.log(`Wind speed is at: ${weatherData.current.wind_mph} mph`);
 }
 
-getWeather("ontario CA");
+async function getTemperature(location, scale){
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=405b9413895a483db38183130242905&q=${location}`, {mode: 'cors'});
+    const tempData = await response.json();
+
+    return (scale == "f") ? tempData.current.temp_f + "°F": tempData.current.temp_c + "°C";
+    // return tempData;
+}
+
+// const temperature = (location) => fetch(`https://api.weatherapi.com/v1/current.json?key=405b9413895a483db38183130242905&q=${location}`, {mode: 'cors'})
+//   .then((response) => response.json())
+//   .then((x) => {
+//     return x.current.temp_f;
+//   });
+
+async function createWeatherObj(location, scale){
+    const weatherObj = {
+        temperature: await getTemperature(location, scale),
+    }
+
+    return weatherObj;
+}
+
+// getWeather("ontario CA");
+
+// getTemperature("canada", "f").then(temp => console.log(temp));
+
+createWeatherObj("ontario CA", "c").then(x => console.log(x));
+
+// console.log(temperature("ontario CA"));
