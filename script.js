a1bae0c0bfc674ece47bbe755d78c2057e5452c2
Joe Chek <jmchek@gmail.com>
2024-06-13 10:52:23 -0700

    finish the createWeatherObj() and finalize the getWeather() function

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
-async function getWeather(location) {
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=405b9413895a483db38183130242905&q=${location}`, {mode: 'cors'});
-    const weatherData = await response.json();
-    
-    console.log(`Current temperature in ${location.charAt(0).toUpperCase() + location.slice(1)}: ` + weatherData.current.temp_f + "°F");
-    console.log(`Precipitation is at: ${weatherData.current.precip_mm}%`);
-    console.log(`Humidity is at: ${weatherData.current.humidity}%`);
-    console.log(`Wind speed is at: ${weatherData.current.wind_mph} mph`);
+async function getWeather(location, tempScale) {
+
+    createWeatherObj(location, tempScale).then( x => {
+        console.log(`Location: ${x.location}`),
+        console.log(`Temperature: ` + x.temperature),
+        console.log(`Precipitation: ${x.precipitation}%`),
+        console.log(`Humidity: ${x.humidity}%`),
+        console.log(`Wind speed: ${x.windSpeed}`)
+    });
 }
 
-async function getTemperature(location, scale){
+async function getTemperature(location, tempScale){
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=405b9413895a483db38183130242905&q=${location}`, {mode: 'cors'});
     const tempData = await response.json();
 
-    return (scale == "f") ? tempData.current.temp_f + "°F": tempData.current.temp_c + "°C";
-    // return tempData;
+    return (tempScale == "f") ? tempData.current.temp_f + "°F": tempData.current.temp_c + "°C";
 }
 
-// const temperature = (location) => fetch(`https://api.weatherapi.com/v1/current.json?key=405b9413895a483db38183130242905&q=${location}`, {mode: 'cors'})
-//   .then((response) => response.json())
-//   .then((x) => {
-//     return x.current.temp_f;
-//   });
+async function getPrecipitation(location){
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=405b9413895a483db38183130242905&q=${location}`, {mode: 'cors'});
+    const precipData = await response.json();
 
-async function createWeatherObj(location, scale){
+    return precipData.current.precip_mm + "%";
+}
+
+async function getHumidity(location){
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=405b9413895a483db38183130242905&q=${location}`, {mode: 'cors'});
+    const humidData = await response.json();
+
+    return humidData.current.humidity + "%";
+}
+
+async function getWindSpeed(location){
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=405b9413895a483db38183130242905&q=${location}`, {mode: 'cors'});
+    const windSpdData = await response.json();
+
+    return windSpdData.current.wind_mph + " mph";
+}
+
+async function createWeatherObj(location, tempScale){
     const weatherObj = {
-        temperature: await getTemperature(location, scale),
+        location: location.charAt(0).toUpperCase() + location.slice(1),
+        temperature: await getTemperature(location, tempScale),
+        precipitation: await getPrecipitation(location),
+        humidity: await getHumidity(location),
+        windSpeed: await getWindSpeed(location)
     }
 
     return weatherObj;
 }
 
-// getWeather("ontario CA");
 
-// getTemperature("canada", "f").then(temp => console.log(temp));
-
-createWeatherObj("ontario CA", "c").then(x => console.log(x));
-
-// console.log(temperature("ontario CA"));
+getWeather("los angeles", "f");
